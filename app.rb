40ce4ac12d9bfd5f5d0695a889b937b56b5193ce
Maria Valero Gonzalez <mariavalerogonzalez@ma376.home>
2021-05-17 15:06:36 +0100

    Commit after first feature test in step 2

--- app.rb
+++ app.rb
@@ -5,9 +5,9 @@ class BookmarkManager < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
-  
+
   get '/' do
-    'Hello World'
+    'Bookmark Manager'
   end
 
   run! if app_file == $0
